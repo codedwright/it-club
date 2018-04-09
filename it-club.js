51061195d366b0d9836db6d32b0f2c588fce940f
codedwright <info@codedwright.com>
2018-04-08 21:08:08 -0400

    feat(js): build out angular smooth scroll and ngmaps

--- it-club.js
+++ it-club.js
@@ -5,7 +5,7 @@
  * Author: Joseph Wright (joseph@codedwright.com).
  */
 
-var app = angular.module('itclub', ['ngRoute']);
+var app = angular.module('itclub', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ngMap']);
 app.controller('AboutController', ['$scope', function($scope) {
 	
 }]);
@@ -18,8 +18,15 @@ app.controller('ContactController', ['$scope', function($scope) {
 app.controller('EventsController', ['$scope', function($scope) {
 	
 }]);
-app.controller('HomeController', ['$scope', function($scope) {
-	
+app.controller('HomeController', ['$scope', 'NgMap', function($scope, NgMap) {
+    
+    // https://ngmap.github.io/     
+    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyANiFWCJ6jQBnnJVez5wBS7tbERHaWm330";
+    NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+    }); 
 }]);
 app.controller('NewsController', ['$scope', function($scope) {
 	
@@ -161,27 +168,6 @@ if ( typeof define === 'function' && define.amd ) {
 }
 
 })( window );
-var myCenter = new google.maps.LatLng(41.1123112, -85.1119422);
-
-function initMap() {
-var mapProp = {
-	center: myCenter,
-	zoom:16,
-	scrollwheel:false,
-	draggable:false,
-	mapTypeId:google.maps.MapTypeId.ROADMAP
-};
-
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-var marker = new google.maps.Marker({
-	position:myCenter,
-});
-
-marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initMap);
 /**
  * borderMenu.js v1.0.0
  * http://www.codrops.com
@@ -252,24 +238,130 @@ google.maps.event.addDomListener(window, 'load', initMap);
 	init();
 
 })();
-$(document).ready(function(){
-	// Add smooth scrolling to all links
-	$("nav a, footer a[href='#top']").on('click', function(event) {
+// $(document).ready(function(){
+// 	// Add smooth scrolling to all links
+// 	$("nav a, footer a[href='#top']").on('click', function(event) {
 
-	// Make sure this.hash has a value before overriding default behavior
-	if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
-			// Store hash
-			var hash = this.hash;
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 800, function() {
-			// Add hash (#) to URL when done scrolling (default click behavior)
-			window.location.hash = hash;
-		});
-		} // End if
-	});
-});
+// 	// Make sure this.hash has a value before overriding default behavior
+// 	if (this.hash !== "") {
+// 			// Prevent default anchor click behavior
+// 			event.preventDefault();
+// 			// Store hash
+// 			var hash = this.hash;
+// 			// Using jQuery's animate() method to add smooth page scroll
+// 			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+// 			$('html, body').animate({
+// 				scrollTop: $(hash).offset().top
+// 			}, 800, function() {
+// 			// Add hash (#) to URL when done scrolling (default click behavior)
+// 			window.location.hash = hash;
+// 		});
+// 		} // End if
+// 	});
+// });
+
+app.directive('smoothScroll', ['$log', '$timeout', '$window',
+	function ($log, $timeout, $window) {
+		/*
+		  Retrieve the current vertical position
+		  @returns Current vertical position
+	  */
+
+		var currentYPosition, elmYPosition, smoothScroll;
+		currentYPosition = function () {
+			if ($window.pageYOffset) {
+				return $window.pageYOffset;
+			}
+			if ($window.document.documentElement && $window.document.documentElement.scrollTop) {
+				return $window.document.documentElement.scrollTop;
+			}
+			if ($window.document.body.scrollTop) {
+				return $window.document.body.scrollTop;
+			}
+			return 0;
+		};
+		/*
+		  Get the vertical position of a DOM element
+		  @param eID The DOM element id
+		  @returns The vertical position of element with id eID
+	  */
+
+		elmYPosition = function (eID) {
+			var elm, node, y;
+			elm = document.getElementById(eID);
+			if (elm) {
+				y = elm.offsetTop;
+				node = elm;
+				while (node.offsetParent && node.offsetParent !== document.body) {
+					node = node.offsetParent;
+					y += node.offsetTop;
+				}
+				return y;
+			}
+			return 0;
+		};
+		/*
+		  Smooth scroll to element with a specific ID without offset
+		  @param eID The element id to scroll to
+		  @param offSet Scrolling offset
+	  */
+
+		smoothScroll = function (eID, offSet) {
+			var distance, i, leapY, speed, startY, step, stopY, timer, _results;
+			startY = currentYPosition();
+			stopY = elmYPosition(eID) - offSet;
+			distance = (stopY > startY ? stopY - startY : startY - stopY);
+			if (distance < 100) {
+				scrollTo(0, stopY);
+				return;
+			}
+			speed = Math.round(distance / 100);
+			if (speed >= 20) {
+				speed = 20;
+			}
+			step = Math.round(distance / 25);
+			leapY = (stopY > startY ? startY + step : startY - step);
+			timer = 0;
+			if (stopY > startY) {
+				i = startY;
+				while (i < stopY) {
+					setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+					leapY += step;
+					if (leapY > stopY) {
+						leapY = stopY;
+					}
+					timer++;
+					i += step;
+				}
+				return;
+			}
+			i = startY;
+			_results = [];
+			while (i > stopY) {
+				setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+				leapY -= step;
+				if (leapY < stopY) {
+					leapY = stopY;
+				}
+				timer++;
+				_results.push(i -= step);
+			}
+			return _results;
+		};
+		return {
+			restrict: 'A',
+			link: function (scope, element, attr) {
+				return element.bind('click', function () {
+					var offset;
+					if (attr.target) {
+						offset = attr.offset || 100;
+						$log.log('Smooth scroll: scrolling to', attr.target, 'with offset', offset);
+						return smoothScroll(attr.target, offset);
+					} else {
+						return $log.warn('Smooth scroll: no target specified');
+					}
+				});
+			}
+		};
+	}
+])
