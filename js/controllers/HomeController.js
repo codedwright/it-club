83a4f1dcbb4a0fff3fc510f97aad1f85d98d7998
codedwright <info@codedwright.com>
2018-04-09 10:58:59 -0400

    feat(events): orgsync api integration Add http get for OrgSync API event JSON data.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,4 +1,4 @@
-app.controller('HomeController', ['$scope', 'NgMap', function($scope, NgMap) {
+app.controller('HomeController', ['$scope', '$http', 'NgMap', function($scope, $http, NgMap) {
     
     // https://ngmap.github.io/     
     $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyANiFWCJ6jQBnnJVez5wBS7tbERHaWm330";
@@ -7,4 +7,8 @@ app.controller('HomeController', ['$scope', 'NgMap', function($scope, NgMap) {
         console.log('markers', map.markers);
         console.log('shapes', map.shapes);
     }); 
+    $http.get("https://api.orgsync.com/api/v3/portals/139217/events.json?key=saQO-P_BwlyykPjd0Et9woSjp_IgtCbnK1NhHcCBdA0&per_page=100&upcoming=true").then((results) => {
+        console.log(results.data);
+        $scope.events = results.data.data;
+    })
 }]);
